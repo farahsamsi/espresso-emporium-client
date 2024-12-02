0b894a83c8efadd6a97959e09e82e4b176fb9978
farahsamsi <1827034farahsamsiprome@gmail.com>
2024-12-02 11:05:18 +0600

    create users post api and save user to mongodb

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -1,7 +1,8 @@
 import React, { useContext, useState } from 'react';
-import { FaEyeSlash, FaRegEye } from 'react-icons/fa6';
+import { FaArrowLeftLong, FaEyeSlash, FaRegEye } from 'react-icons/fa6';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../Providers/AuthProvider';
+import Swal from 'sweetalert2';
 
 const SignUp = () => {
 
@@ -17,40 +18,67 @@ const SignUp = () => {
 
         createUser(email, password)
             .then(result => {
-                console.log(result.user)
+                const createdAt = result?.user?.metadata?.creationTime;
+                const newUser = { name, email, photo, createdAt }
+                fetch('http://localhost:5000/users', {
+                    method: 'POST',
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify(newUser)
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.insertedId) {
+                            Swal.fire({
+                                title: 'Success',
+                                text: 'User added successfully',
+                                icon: 'success',
+                                confirmButtonText: 'Okay'
+                            })
+                        }
+                    })
+
             })
             .catch(error => console.log(error.message))
+
 
     }
 
     const [showPassword, setShowPassword] = useState(false);
 
     return (
-        <div className="my-6">
+        <div className='w-11/12 container mx-auto my-6'>
+            <div className='my-5 lg:my-12'>
+                <Link to={-1} className='flex items-center gap-3'>
+                    <FaArrowLeftLong />
+                    <h1 className='font-rancho text-xl'>Back to home</h1>
+                </Link>
+            </div>
             <div className="md:w-8/12 mx-auto flex flex-col justify-center items-center text-center gap-4 mb-5 ">
                 <h1 className="text-2xl lg:text-5xl font-extrabold font-rancho">Register Now</h1>
                 <p className="font-medium text-black/80">Get all Discount Pro coupons with a single click by registering in simple steps.</p>
             </div>
 
-            <div className="card bg-base-100 w-11/12 mx-auto lg:max-w-screen-md shrink-0 shadow-2xl border relative">
+            <div className="card bg-[#F4F3F0] w-11/12 mx-auto lg:max-w-screen-md shrink-0 relative">
                 <form onSubmit={handleSubmit} className="card-body">
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Name</span>
                         </label>
-                        <input name="name" type="text" placeholder="Enter your name" className="input input-bordered" required />
+                        <input name="name" type="text" placeholder="Enter your name" className="input" required />
                     </div>
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Email</span>
                         </label>
-                        <input name="email" type="email" placeholder="Enter your email" className="input input-bordered" required />
+                        <input name="email" type="email" placeholder="Enter your email" className="input" required />
                     </div>
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Photo URL</span>
                         </label>
-                        <input name="photo" type="text" placeholder="Enter your display photo URL" className="input input-bordered" required />
+                        <input name="photo" type="text" placeholder="Enter your display photo URL" className="input" required />
                     </div>
                     <div className="form-control">
                         <label className="label">
@@ -58,7 +86,7 @@ const SignUp = () => {
                         </label>
                         <input name="password"
                             type={showPassword ? 'text' : 'password'}
-                            placeholder="password" className="input input-bordered" required />
+                            placeholder="password" className="input" required />
 
                     </div>
                     {/* {
@@ -79,16 +107,14 @@ const SignUp = () => {
                 <div className="flex flex-col lg:flex-row gap-4 justify-around items-center mb-6">
                     <div className="flex flex-col justify-center">
                         <p>Already have an account ?</p>
-                        <Link to='/signIn' className="btn"><button>Login</button></Link>
+                        <Link to='/signIn' className="btn bg-white border-none"><button>Sign In</button></Link>
                     </div>
                     <div className="flex flex-col justify-center items-center">
                         <p>Or</p>
-                        <button className="btn">Continue with Google
+                        <button className="btn bg-white border-none">Continue with Google
                             <img width="48" height="48" src="https://img.icons8.com/color/48/google-logo.png" alt="google-logo" />
                         </button>
                     </div>
-
-
                 </div>
             </div>
         </div>
